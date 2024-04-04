715fd6830a325138a5c85790a40241db0bae84e7
yeshedj <157068258+yeshedj@users.noreply.github.com>
2024-04-04 18:29:18 -0500

    RESTORED FILES

---
+++ src/breakout/Paddle.java
@@ -0,0 +1,18 @@
+// Yeshe Jangchup
+// The Paddle class creates the rectangle paddle shape
+// Acknowledements: Tenzin
+
+package breakout;
+import edu.macalester.graphics.Rectangle;
+
+/*
+ * Creates a rectangular paddle shape
+ */
+public class Paddle extends Rectangle{
+    private static final double WIDTH = 90;
+    private static final double HEIGHT = 10;
+
+    public Paddle(double canvasWidth, double canvasHeight){
+        super((canvasWidth - WIDTH), (canvasHeight - HEIGHT) - 20, WIDTH, HEIGHT);
+    }
+}
