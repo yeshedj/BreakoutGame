2b3814b870ede38511b62fb65479ad320f62e042
yeshedj <157068258+yeshedj@users.noreply.github.com>
2024-04-04 18:21:39 -0500

    Revert "Merge pull request #2 from mac-comp127-marshs24/revert-1-main"
    This reverts commit 6e5ea1bd97b236534496b15d9b7747cdd65cc738, reversing
    changes made to b414048b1c6d17c9353a9cd047bf5420cf2476b9.

--- src/breakout/BreakoutGame.java
+++ src/breakout/BreakoutGame.java
@@ -1,22 +1,77 @@
+// Yeshe Jangchup
+// The BreakOut Game initializes the game window, sets up the paddle, ball, and bricks. It handles the movement of the paddle based on the user's mouse moving and also handles the animation of the ball.
+// Acknowledgements: Stephanie, William, Tenzin
+
 package breakout;
+import java.awt.Color;
+import java.util.ArrayList;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
 
 /**
- * The game of Breakout.
+ * The game of Breakout. 
+ * Sets up the paddle, ball, and bricks.
  */
 public class BreakoutGame {
-    private static final int CANVAS_WIDTH = 600;
-    private static final int CANVAS_HEIGHT = 800;
-
     private CanvasWindow canvas;
+    private Rectangle paddle;
+    private Ball ball;
+
+    private BrickHandler brickHandler;
+    ArrayList<Brick> bricks;
+
+
+    private static final int CANVAS_WIDTH = 650;
+    private static final int CANVAS_HEIGHT = 750;
+
+    double centerX = CANVAS_WIDTH / 2.0;  
+    double centerY = CANVAS_HEIGHT / 2.0;
+
+    double initialSpeedX = 5.0;
+    double initialSpeedY = 5.0;
+
+    Color NEW_COLOR = new Color(220, 200, 250);
 
     public BreakoutGame() {
         canvas = new CanvasWindow("Breakout!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.setBackground(Color.BLACK);
+      
+        brickHandler=new BrickHandler(canvas);
+        brickHandler.createBricks();
 
+        paddle = new Rectangle(CANVAS_WIDTH / 2 - 50, CANVAS_HEIGHT - 50, 100, 10);
+        paddle.setFillColor(NEW_COLOR);
+        canvas.add(paddle);        
+
+        ball = new Ball (centerX, centerY, initialSpeedX, initialSpeedY, CANVAS_WIDTH, CANVAS_HEIGHT, brickHandler);
+        ball.addToCanvas(canvas);
+        animateBall();
+
+        canvas.onMouseMove(event -> {
+            double newX = event.getPosition().getX();
+            if (newX >= 0 && newX <= canvas.getWidth() - paddle.getWidth()) {
+                paddle.setX(newX);
+            }
+        });
     }
 
-    public static void main(String[] args){
+    /*
+     * Animates the movement of the ball on the canvas. 
+     * Responds to collision, changes direction, and interacts with other elements
+     */
+    public void animateBall(){
+        canvas.animate(()->{
+            ball.move(canvas, paddle, brickHandler);
+            // ball.brickCollide(canvas, brickHandler);
+            // ball.paddleCollide(canvas, paddle);
+        });
+    }
+
+    /*
+     * Initializes and starts the game when the program runs
+     */
+  public static void main(String[] args){
         new BreakoutGame();
     }
 }
---
+++ src/breakout/BrickHandler.java
@@ -0,0 +1,64 @@
+// Yeshe Jangchup
+// The BrickHandler class involves the creation of the bricks and handling them. It displays a grid of bricks on the canvas and keeps track of the bricks
+// Acknowledgements: Rocky
+
+package breakout;
+
+import edu.macalester.graphics.CanvasWindow;
+
+/*
+ * Creates the bricks and handles brick objects
+ */
+public class BrickHandler{    
+    private CanvasWindow canvas;
+
+    int brickCount = 0;
+
+    public static final int BRICK_WIDTH = 65;
+    public static final int BRICK_HEIGHT = 20;
+    public static final int BRICK_SPACING = 7;
+
+    public BrickHandler(CanvasWindow canvas) {
+        this.canvas = canvas;
+    }
+
+    /*
+     * Generates a grid of bricks on the canvas, 
+     * iterating over rows and columns to determine positoining
+     */
+    public void createBricks() {
+        int rows = 9;
+        int cols = 9;
+
+        brickCount = rows*cols;
+
+        int startX = 5; 
+        int startY = 55; 
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int x = startX + (BRICK_WIDTH + BRICK_SPACING) * col;
+                int y = startY + (BRICK_HEIGHT + BRICK_SPACING) * row;
+                Brick brick = new Brick(x, y, canvas);
+                brick.setFillColor();
+                brick.setStrokeColor();
+                brick.addToCanvas();
+            }
+        }        
+    }
+
+    /*
+     * Returns the number of bricks in the bricks list.
+     */
+    public int getNumOfBricks() {
+        return brickCount;
+    }
+
+    /*
+     * Decrements the brickCount variable
+     */
+    public void removeBrickFromList(Ball ball){
+        brickCount --;
+
+    }
+}
